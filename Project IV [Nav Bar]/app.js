92af44381ae65ae317208b0270d59911660b2dea
Shubhankar <shubhankar.sharma97@gmail.com>
2024-02-26 14:31:01 +0000

    4th Project Done

--- Project IV [Nav Bar]/app.js
+++ Project IV [Nav Bar]/app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggleBtn=document.querySelector(".nav-toggle");
+const links=document.querySelector(".links");
+
+
+navToggleBtn.addEventListener("click",()=>{
+
+    // Naive Method
+//    if(links.classList.contains("show-links")==false)
+//         links.classList.add("show-links");
+//     else
+//         links.classList.remove("show-links");
+
+    //best method
+    links.classList.toggle("show-links");
+})
